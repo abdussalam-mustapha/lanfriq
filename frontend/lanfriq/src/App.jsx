0d32c6aaf8604b54a750851ec0ade2e658dfd5a7
abdussalam-mustapha <abdussalammustapha07@gmail.com>
2025-11-24 17:56:46 +0100

    chore: update folder structure

--- frontend/lanfriq/src/App.jsx
+++ frontend/lanfriq/src/App.jsx
@@ -1,25 +1,16 @@
 import { ThemeProvider } from './context/ThemeContext'
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
-import Hero from './components/sections/Hero'
-import Features from './components/sections/Features'
-import TargetAudience from './components/sections/TargetAudience'
-import Statistics from './components/sections/Statistics'
-import HowItWorks from './components/sections/HowItWorks'
-import CTA from './components/sections/CTA'
+import LandingPage from './pages/landing/LandingPage'
 import './App.css'
 
 function App() {
   return (
     <ThemeProvider>
       <div className="app">
+        <Header />
         <main>
-          <Hero />
-          <Features />
-          <TargetAudience />
-          <Statistics />
-          <HowItWorks />
-          <CTA />
+          <LandingPage />
         </main>
         <Footer />
       </div>
---
+++ frontend/lanfriq/src/pages/landing/LandingPage.jsx
@@ -0,0 +1,21 @@
+import Hero from '../components/sections/Hero'
+import Features from '../components/sections/Features'
+import TargetAudience from '../components/sections/TargetAudience'
+import Statistics from '../components/sections/Statistics'
+import HowItWorks from '../components/sections/HowItWorks'
+import CTA from '../components/sections/CTA'
+
+const LandingPage = () => {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <TargetAudience />
+      <Statistics />
+      <HowItWorks />
+      <CTA />
+    </>
+  )
+}
+
+export default LandingPage
